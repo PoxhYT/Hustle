7b3e8e76ea41fc730ccc35de9c039db6d3eec3e5
PoxhYT <72230632+PoxhYT@users.noreply.github.com>
2024-04-08 11:21:29 +0200

    Update widget_test.dart

--- hustle/test/widget_test.dart
+++ hustle/test/widget_test.dart
@@ -1,23 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:hustle/screens/main_screen.dart';
 
-const MessagesCollection = 'messages';
-
 void main() {
-  testWidgets('shows messages', (WidgetTester tester) async {
+  testWidgets('Test create new todo', (WidgetTester tester) async {
     // Populate the fake database.
     final firestore = FakeFirebaseFirestore();
-    await firestore.collection(MessagesCollection).add({
-      'message': 'Hello world!',
-      'created_at': FieldValue.serverTimestamp(),
-    });
 
     // Render the widget.
-    await tester.pumpWidget(MaterialApp(
-        title: 'Firestore Example', home: MainScreen(firebaseFirestore: firestore)));
+    await tester.pumpWidget(
+        MaterialApp(home: MainScreen(firebaseFirestore: firestore)));
     // Let the snapshots stream fire a snapshot.
     await tester.idle();
     // Re-render.
